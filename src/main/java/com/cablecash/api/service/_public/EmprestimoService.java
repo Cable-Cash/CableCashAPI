6b0335da4d01467319805c693e94d4d0b71d272e
tleitejr <juniorleitetonin@gmail.com>
2024-08-14 22:13:06 -0300

    Criado endpoint GET de Emprestimo

---
+++ src/main/java/com/cablecash/api/service/_public/EmprestimoService.java
@@ -0,0 +1,19 @@
+package com.cablecash.api.service._public;
+
+import com.cablecash.api.model.dto._public.EmprestimoDTO;
+import com.cablecash.api.repository._public.EmprestimoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Stream;
+
+@Service
+public class EmprestimoService {
+
+    @Autowired
+    EmprestimoRepository repository;
+
+    public Stream<EmprestimoDTO> getAllEmprestimos() {
+        return repository.findAll().stream().map(EmprestimoDTO::new);
+    }
+}
